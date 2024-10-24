c7fe48d9f945e0eb2202a532e3953d1ec1d2a86e
Svetlana Dubkova <svetl@MacBook-Air-2.local>
2024-10-23 22:17:25 -0400

    try

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="0; url=https://skovaphotos.com">
-    <title>Redirecting...</title>
-</head>
-<body>
-    <p>If you are not redirected, <a href="https://skovaphotos.com">click here</a>.</p>
-</body>
-</html>
+import { redirect } from 'next/navigation'
+
+export default function HomePage() {
+  redirect('https://skovaphotos.com')
+}
