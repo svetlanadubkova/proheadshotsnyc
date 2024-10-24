0bfc983401f4654b059fa8668b5c7904bb89df97
Svetlana Dubkova <svetl@MacBook-Air-2.local>
2024-10-23 23:06:30 -0400

    new

---
+++ app/pricing/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'headshots by lana dubkova',
+    description: 'nyc headshots | brooklyn & manhattan',
+  }
+  
+  export default function PricingLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return children
+  }
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -3,10 +3,6 @@
 import Link from 'next/link'
 import { useState } from 'react'
 
-export const metadata = {
-  title: 'headshots by lana dubkova',
-  description: 'nyc headshots | brooklyn & manhattan',
-}
 
 type Tier = {
   title: string;
